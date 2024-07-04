faafb926cfde70b78bfad0173a07f23860d7734a
nafisholeh <rachmadnafisholeh@gmail.com>
2024-07-04 16:27:45 +0700

    fix: add responsiveness to trade button

--- components/TradeButton.tsx
+++ components/TradeButton.tsx
@@ -4,7 +4,9 @@ export default function TradeButton() {
   const dummyClick = () => {};
 
   return (
-    <button onClick={dummyClick} className="bg-button-secondary text-action p-[10px]">
+    <button
+      onClick={dummyClick}
+      className="bg-button-secondary text-xl sm:text-2xl text-action p-[10px] w-full xs:w-auto">
       Trade
     </button>
   );
